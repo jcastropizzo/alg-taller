366263e405dd01e77883bc0d543909e5884181a4
Julian Castro <julian.castro@n5now.com>
2021-05-19 22:42:11 -0300

    TP1-V2

--- TP1/TN-CASTROPIZZO.hs
+++ TP1/TN-CASTROPIZZO.hs
@@ -1,45 +1,29 @@
-first ::(a, b, c) -> a
-first (a, b, c) = a
-second ::(a, b, c) -> b
-second (a, b, c) = b
-third ::(a, b, c) -> c 
-third (a, b, c) = c
-
-
 esPar :: Integer -> Bool
 esPar n = ( n `mod` 2) == 0
 
 esPrimo :: Integer -> Bool
-esPrimo num
-    | num < 0 = False
-    | num == 0 = True
-    | otherwise = esPrimoIterador (num,num)
-    where
-        esPrimoIterador :: (Integer, Integer) -> Bool
-        esPrimoIterador (baseNum, currentNum)
-            | currentNum <= 1 = True -- Es primo, llegó al final del loop
-            | baseNum == currentNum = esPrimoIterador (baseNum, currentNum-1)
-            | ((baseNum `mod` currentNum) == 0)
-                && (baseNum /= currentNum) = False
-            | otherwise = esPrimoIterador (baseNum, currentNum-1)
+esPrimo n
+    | n < 2 = False
+    | otherwise = ((menorDivisor n) == n)
+
+menorDivisor :: Integer -> Integer
+menorDivisor n = menorDivisorDesde (2, n)
+
+menorDivisorDesde :: (Integer, Integer) -> Integer
+menorDivisorDesde (k, n)
+    | mod n k == 0 = k
+    | otherwise = menorDivisorDesde ((k + 1), n)
 
 esSumaDeDosPrimos :: Integer -> Bool
 esSumaDeDosPrimos num
-    | num <= 2 = True
-    | otherwise = esSumaDeDosPrimosPrimerIterador (0, num)
+    | num <= 2 = False
+    | otherwise = esSumaDeDosPrimosIterador (2, num)
     where
-        esSumaDeDosPrimosPrimerIterador :: (Integer, Integer) -> Bool
-        esSumaDeDosPrimosPrimerIterador (x, objetivo)
-            | (x >= objetivo) = esSumaDeDosPrimosSegundoIterador (x, 0, objetivo)
-            | not (esPrimo x) = esSumaDeDosPrimosPrimerIterador (x+1, objetivo)
-            | esSumaDeDosPrimosSegundoIterador (x, 0, objetivo) = True
-            | otherwise = esSumaDeDosPrimosPrimerIterador (x+1, objetivo)
-        esSumaDeDosPrimosSegundoIterador :: (Integer, Integer, Integer) -> Bool
-        esSumaDeDosPrimosSegundoIterador (x, currentY, objetivo)
-            | currentY >= objetivo = False
-            | (esPrimo x) && (esPrimo currentY)
-                && ((x + currentY) == objetivo) = True
-            | otherwise = esSumaDeDosPrimosSegundoIterador (x, currentY + 1, objetivo)
+        esSumaDeDosPrimosIterador :: (Integer, Integer) -> Bool
+        esSumaDeDosPrimosIterador (x, objetivo)
+            | ((2*x) > objetivo) = False -- si ya validó todos los x,y con 2 <= x < objetivo/2, se probaron todas las combinaciones hasta la x,x
+            | (esPrimo x) && (esPrimo (objetivo-x)) = True
+            | otherwise = esSumaDeDosPrimosIterador ((x+1), objetivo)
 
 -- EJ 1
 satisfaceGoldbach :: Integer -> Bool
@@ -48,44 +32,35 @@ satisfaceGoldbach n
     | otherwise = False
 
 -- EJ 2
+-- Precondiciones:
+--    Entrada: n > 2
 verificarConjeturaHasta :: Integer -> Bool
 verificarConjeturaHasta n
-    | n <= 2 = False
-    | n == 3 = True
     | (n `mod` 2) /= 0 = verificarConjeturaHasta (n-1)
-    | otherwise = (satisfaceGoldbach n) && verificarConjeturaHasta (n-1)
+    | n == 2 = True
+    | satisfaceGoldbach n = verificarConjeturaHasta (n-2)
+    | otherwise = False
 
 -- EJ 3
+-- Precondiciones:
+--    Entrada: n > 2
 descomposicionEnPrimos :: Integer -> (Integer,Integer)
-descomposicionEnPrimos n
-    | (n <= 2) || ((n `mod` 2) /= 0) = (-1,-1) -- tiene que ser mayor a 2 y par
-    | otherwise = descomposicionEnPrimosIteradorEnX (0,n)
+descomposicionEnPrimos n = descomposicionEnPrimosIterador (2,n)
     where 
-        descomposicionEnPrimosIteradorEnX :: (Integer, Integer) -> (Integer, Integer)
-        descomposicionEnPrimosIteradorEnX (x, objetivo)
-            | x >= objetivo = (-1,-1)
-            | y /= -1 = (x, y)
-            | otherwise = descomposicionEnPrimosIteradorEnX(x+1,objetivo)
-            where y = descomposicionEnPrimosIteradorEnY(x,objetivo,0)
-        descomposicionEnPrimosIteradorEnY :: (Integer, Integer, Integer) -> Integer
-        descomposicionEnPrimosIteradorEnY (x, objetivo, currentY)
-            | (esPrimo x) && (esPrimo currentY) && ((x + currentY) == objetivo) = currentY
-            | (currentY + x) >= objetivo = -1
-            | otherwise = descomposicionEnPrimosIteradorEnY (x,objetivo,currentY+1)
+        descomposicionEnPrimosIterador :: (Integer, Integer) -> (Integer, Integer)
+        descomposicionEnPrimosIterador (x, objetivo)
+            | x > objetivo = (-1,-1) -- guarda para evitar loop infinito si no tiene descomposición en primos
+            | (esPrimo x) && (esPrimo (objetivo-x)) = (x, objetivo-x)
+            | otherwise = descomposicionEnPrimosIterador(x+1, objetivo)
 
-
+-- EJ 4
+-- Precondiciones:
+--    Entrada: n > 2
 numeroDeDescomposiciones :: Integer -> Integer
-numeroDeDescomposiciones n
-    | (n <= 2) || ((n `mod` 2) /= 0) = -1 -- tiene que ser mayor a 2 y par
-    | otherwise = descomposicionEnPrimosIteradorEnX (0,n)
+numeroDeDescomposiciones n = descomposicionEnPrimosIterador (2, n)
     where 
-        descomposicionEnPrimosIteradorEnX :: (Integer, Integer) -> Integer
-        descomposicionEnPrimosIteradorEnX (x, objetivo)
-            | x >= objetivo = 0
-            | esPrimo x = descomposicionEnPrimosIteradorEnY (x,objetivo,1) + descomposicionEnPrimosIteradorEnX(x+1,objetivo)
-            | otherwise = descomposicionEnPrimosIteradorEnX(x+1,objetivo)
-        descomposicionEnPrimosIteradorEnY :: (Integer, Integer, Integer) -> Integer
-        descomposicionEnPrimosIteradorEnY (x, objetivo, currentY)
-            | (esPrimo x) && (esPrimo currentY) && ((x + currentY) == objetivo) = 1
-            | (currentY + x) >= objetivo = 0
-            | otherwise = descomposicionEnPrimosIteradorEnY (x,objetivo,currentY+1)
+        descomposicionEnPrimosIterador :: (Integer, Integer) -> Integer
+        descomposicionEnPrimosIterador (x, objetivo)
+            | (objetivo - x) < 2 = 0 -- no tiene sentido seguir el loop, siendo 2 el primo más pequeño
+            | esPrimo x && esPrimo (objetivo - x) = 1 + descomposicionEnPrimosIterador (x + 1, objetivo)
+            | otherwise = descomposicionEnPrimosIterador (x + 1, objetivo)
